36290918d91e2d01d2de048027e2f181f875c986
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-08 19:32:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,26 +11,21 @@ Car.prototype.getMakeModel = function () {
 
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this, make, model);
-  this.topSpeed = topSpeed;
-}
+	 Car.call(this, make, model);
+      this.topSpeed = topSpeed;
+    }
 
-// Set up inheritance from Car prototype
-SportsCar.prototype = Object.create(Car.prototype);
+    SportsCar.prototype = Object.create(Car.prototype);
+    SportsCar.prototype.constructor = SportsCar;
 
-// Ensure the constructor is set correctly
-SportsCar.prototype.constructor = SportsCar;
+    SportsCar.prototype.getTopSpeed = function () {
+      return this.topSpeed;
+    };
 
-// Add method to SportsCar prototype
-SportsCar.prototype.getTopSpeed = function () {
-  return this.topSpeed;
-};
-
-// Test Case
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-
-console.log(car.getMakeModel()); // Output: Ferrari Testarossa
-console.log(car.getTopSpeed());
+    // Expose constructors to global window object (for Cypress)
+    window.Car = Car;
+    window.SportsCar = SportsCar;
+	
 }
 
 // Do not change the code below
